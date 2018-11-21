05f26d58b8863efc665a8fea486786a20ad25a80
Dan Cummings <24532391+dcummingsmb@users.noreply.github.com>
2018-11-20 19:25:16 -0600

    redirected api calls to heroku deployment

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ class App extends Component {
     //If we use this.state.imageURL here, it errors
     //this is because of the way setState works, it's frustrating but it is what it is
     //just remember, don't set state, and then try to use that state.property in the same function
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://face-det-api.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -101,7 +101,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://face-det-api.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
